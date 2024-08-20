db2e4bbb772d1297ccf4e2a0013e6064cfc41530
matiAlfaro <mati@marvell.com>
2024-08-20 18:17:07 +0300

    restore legal notice
    Signed-off-by: matiAlfaro <mati@marvell.com>

--- sai_poe_library/PDLIB/parser/pdlParser.h
+++ sai_poe_library/PDLIB/parser/pdlParser.h
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2024, MARVELL. All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *    not use this file except in compliance with the License. You may obtain
+ *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ *    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ *    FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ *    See the Apache Version 2.0 License for specific language governing
+ *    permissions and limitations under the License.
+ *
+ */
 /* ---------------------------------------------------------------------------------------------------------------
    Generated from sai_poe_yang_module based on revision 2024-04-01:1.01, PoE support added. on 20/08/2024 18:12:26
    --------------------------------------------------------------------------------------------------------------- */
